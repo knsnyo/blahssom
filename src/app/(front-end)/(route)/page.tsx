a40bcb8a561a3da47c95728ec6d5d7bf8d79d13d
knsnyo <isc963@naver.com>
2024-05-25 23:41:29 +0900

    [update] fix error

--- src/app/(front-end)/(route)/page.tsx
+++ src/app/(front-end)/(route)/page.tsx
@@ -1,30 +1,5 @@
-'use client'
-
-import { IUser } from 'src/@types/user'
-import Feature from 'src/app/(front-end)/__features'
-
 const Page = () => {
-  const dispatch = Feature.Hooks.useAppDispatch()
-
-  const {
-    loading,
-    data: user,
-    error,
-  } = Feature.Hooks.useQuery<IUser>(() => {
-    return Feature.User.Api.getProfile()
-  })
-
-  if (loading) {
-    return <div />
-  }
-
-  if (error) {
-    dispatch(Feature.User.Action.signOut())
-    return <div />
-  }
-
-  dispatch(Feature.User.Action.signIn(user))
-  return <div />
+  return <></>
 }
 
 export default Page
